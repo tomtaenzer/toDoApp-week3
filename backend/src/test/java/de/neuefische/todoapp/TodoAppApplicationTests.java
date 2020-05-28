57406a1891fe8d0d20f5ec1f9b99eba1fe636f7b
fabianschmauder <fabianschmauder@gmail.com>
2020-05-28 09:53:42 +0200

    Init test

---
+++ backend/src/test/java/de/neuefische/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
