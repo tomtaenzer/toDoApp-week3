ee28c8927365e386787abaf6e500b51a7f3d54c9
fabianschmauder <fabianschmauder@gmail.com>
2020-05-29 08:39:23 +0200

    Use long Json syntax

--- frontend/src/service/todo-service.js
+++ frontend/src/service/todo-service.js
@@ -5,7 +5,9 @@ export const getTodos = async () => {
 };
 
 export const addTodo = async (description) => {
-    return await axios.put('/api/todo', {description});
+    return await axios.put('/api/todo', {
+        description: description
+    });
 };
 
 export const deleteTodo = async (id) => {
@@ -13,6 +15,8 @@ export const deleteTodo = async (id) => {
 };
 
 export const setTodoStatus = async (id, status) => {
-    return await axios.put(`/api/todo/${id}/status`, {status});
+    return await axios.put(`/api/todo/${id}/status`, {
+        status: status
+    });
 };
 
